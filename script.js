8ed1e1b317d960b49dfdd881d8e3f5ad102a39bc
Arun <arunmahi921@gmail.com>
2022-06-01 08:50:20 +0530

    completed

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let id= (id) =>document.getElementById(id);
-let classes=(classes) =>document.getElementsByClassName(classes)
 
+let id= (id) =>document.getElementById(id);
+let classes=(classes) =>document.getElementsByClassName(classes);
 
 let userName=id('username'),
 email=id('email'),
 form=id('form'),
+submit=id('submit'),
+hide=id('hide'),
+
  password=id('password'),
- 
-errorMsg = classes('error'),
+ confirmPassword=id('confirmPassword'),
+ errorMsg = classes('error'),
 successIcon=classes('success-icon'),
 failureIcon=classes('failure-icon');
 
@@ -32,9 +35,6 @@ failureIcon=classes('failure-icon');
 // });
 
 
-
-
-
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
 // errorMsg[0].innerHTML='user name cant be empty';
@@ -53,21 +53,60 @@ form.addEventListener('submit',(e)=>{
 
 // }
 
-engine(userName,0,'Username cannot be empty');
-engine(email,1,'Email cant be empty');
-engine(password,2,'Password is required');
+ engine(username,0,'Username cannot be empty',3,6);
+ engine(email,1,'Email cant be empty',15,20);
+engine(password,2,'Password is required',8,15);
+
+if(confirmPassword.value.trim() ===""){
+  errorMsg[3].innerHTML="field can't be empty";
+  failureIcon[3].style.opacity='1';
+  successIcon[3].style.opacity='0'
+
+}else if(confirmPassword.value!==password.value){
+  errorMsg[3].innerHTML="password not match";
+  failureIcon[3].style.opacity='1';
+  successIcon[3].style.opacity='0'
+}else{
+  errorMsg[3].innerHTML="";
+  failureIcon[3].style.opacity='0';
+  successIcon[3].style.opacity='1';
+}
+
+
 
 })
 
+let show=function(){
 
-let engine=(id,serial,message)=>{
-  if(id.value .trim() === ''){
+
+   if (password.type==='password'){
+    password.type="text";
+    // hide.style.opacity="1";
+    hide.style.display=block;
+   
+  }
+  else if (password.type==='text'){
+    password.type="password";
+    // hide.style.opacity="1";
+    hide.style.display=none;
+   
+  }
+
+}
+
+let engine=(id,serial,message,min,max)=>{
+  if(id.value.trim() === ''){
     errorMsg[serial].innerHTML=message;
     failureIcon[serial].style.opacity='1'
     successIcon[serial].style.opacity='0';
-    
-    
-    }else{
+  }
+  else if(id.value.length>max){
+    errorMsg[serial].innerHTML=`${id.name} length is more`;
+  }
+  else if(id.value.length<min){
+    errorMsg[serial].innerHTML= `${id.name} length is less`;
+  }
+  else{
     errorMsg[serial].innerHTML='';
     failureIcon[serial].style.opacity='0'
     successIcon[serial].style.opacity='1';
@@ -77,5 +116,6 @@ let engine=(id,serial,message)=>{
     
 
 }
+
 
 
